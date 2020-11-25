5e17ec414757b83c40180f7e9102f230142d7158
BeNrn <56606686+BeNrn@users.noreply.github.com>
2020-11-25 10:33:25 +0100

    clean cluster analysis code

--- 05_ClusterAnalysis.R
+++ 05_ClusterAnalysis.R
@@ -5,6 +5,9 @@ library(forcats)
 library(ecodist)
 library(magrittr)
 library(tidyverse)
+library(factoextra)
+library(cluster)
+library(gridExtra)
 
 #-------------------------------------------------------------------------------
 #1 LOAD THE DATA
@@ -65,181 +68,77 @@ smp_totalDF <- df[sample(1:nrow(df), 100),]
 #-------------------------------------------------------------------------------
 #2 CLUSTER ANALYSIS - HIERARCHICAL CLUSTERING
 #-------------------------------------------------------------------------------
+#see:
+# - https://uc-r.github.io/hc_clustering
+# - Handl, A., & Kuhlenkasper, T. (2017). Multivariate Analysemethoden. https://doi.org/10.1007/978-3-662-54754-0 (Kapitel 13 - Clusteranalyse)
+
 #gamma coefficient function
 source(paste0(workingDir, "CloudType_and_Precipitation_Analysis_Repository/FUN_Gammacoefficient.R"))
 
-#2.1 Example
-#-----------
-#2.1.1 load the data
-alter <- c(43, 38, 6, 47, 37, 9)
-#2.1.2 calculate the discance matrix
-d <- dist(alter)
-#2.1.3 hierarchical clustering
-#method: distinguishes among others single-linkage, complete-linkage and average-linkage
-e <- hclust(d, method = "complete", members = NULL)
-#2.1.4 Quality of the clustering
-#cophenetic distance matrix
-coph <- cophenetic(e)
-coph <- full(coph)
-#full distance matrix
-dm <- ecodist::full(d)
-#correlation
-cor(dm[lower.tri(dm)], coph[lower.tri(coph)])
-#gamma coefficient
-gammakoeffizient(dm[lower.tri(dm)], coph[lower.tri(coph)])
-#Morjans Test
-1+sum((e$height-mean(e$height))/sqrt(e$height)>1.25)
-#Treppen plot
-ch <- e$height
-dat <- alter
-resolution <- 1
-plot(rep(1,2),c(0,ch[1]),xaxt="n",yaxt="n",xlim=c(0,length(dat)),
-     xaxs="i",yaxs="i",ylim=c(0,max(dat)),type="l",
-     xlab="Anzahl Gruppen",ylab="Verschmelzungsniveau")
-for(i in 2:length(ch)) lines(c(i,i),c(ch[i-1],ch[i]))
-for (i in seq(1:(length(ch)-1))) lines(c(i,i+1),rep(ch[i],2))
-#add axis labels
-#xaxis
-axis(1, at = 0:length(dat),labels=length(dat):0)
-#yaxis
-axis(2, at = seq(0,max(dat),resolution), labels = seq(0,max(dat),resolution))
-
-#2.2 Anwendung, nur mit Regendaten
-#---------------------------------
-#2.2.1 load the data
-smp
-#2.2.2 calculate the discance matrix
-distMat <- dist(smp)
-#2.2.3 hierarchical clustering
-cluster <- hclust(distMat, method = "complete", members = NULL)
-plot(cluster)
-#2.2.4 Quality of the clustering
-#cophenetic distance matrix
-cophMat <- cophenetic(cluster)
-cophMat <- full(cophMat)
-#full distance matrix
-distMat <- ecodist::full(distMat)
-#correlation
-cor(distMat[lower.tri(distMat)], cophMat[lower.tri(cophMat)])
-#gamma coefficient
-gammakoeffizient(distMat[lower.tri(distMat)], cophMat[lower.tri(cophMat)])
-#Morjans Test
-1+sum((cluster$height-mean(cluster$height))/sqrt(cluster$height)>1.25)
-#Treppen plot
-ch <- cluster$height
-dat <- smp
-resolution <- 0.1
-plot(rep(1,2),c(0,ch[1]),xaxt="n",yaxt="n",xlim=c(0,length(dat)),
-     xaxs="i",yaxs="i",ylim=c(0,max(dat)),type="l",
-     xlab="Anzahl Gruppen",ylab="Verschmelzungsniveau")
-for(i in 2:length(ch)) lines(c(i,i),c(ch[i-1],ch[i]))
-for (i in seq(1:(length(ch)-1))) lines(c(i,i+1),rep(ch[i],2))
-#add axis labels
-#xaxis
-axis(1, at = 0:length(dat),labels=length(dat):0)
-#yaxis
-axis(2, at = seq(0,max(dat),resolution), labels = seq(0,max(dat),resolution))
-
-#2.3 Anwendung kompletter df
-#---------------------------------
-#see also https://uc-r.github.io/hc_clustering
-#2.2.1 load the data
+#2.1 load the data
+#remove all variables that only have one characteristic 
+smp_totalDF$acquisitionDate <- NULL
+smp_totalDF$weather <- NULL
+#remove all variables that should not be considered in the cluster analysis  
 smp_totalDF$cloudType <- NULL
 smp_totalDF$lat <- NULL
 smp_totalDF$lon <- NULL
-smp_totalDF$acquisitionDate <- NULL
-smp_totalDF$weather <- NULL
-#2.2.2 calculate the discance matrix
+
+#2.2 calculate the discance matrix
 distMat <- dist(smp_totalDF)
-#2.2.3 hierarchical clustering
+#2.3 hierarchical clustering
 cluster <- hclust(distMat, method = "complete", members = NULL)
-################################################################################
-#TEST
-#plot
+
+#2.4 plot
 plot(cluster)
 
-#divide the data in k groups by its height in the dendrogram
-sub_grp <- cutree(cluster, k = 6)
-table(sub_grp)
-#add a column with group 
-smp_totalDF %>% mutate(cluster = sub_grp) %>% head
-#plot rectangulars
+#2.5 divide the data in k groups by its height in the dendrogram
 plot(cluster)
 rect.hclust(cluster, k = 6, border = 2:6)
 
+#2.6 divide data in subgroups and add this information to the df
+sub_grp <- cutree(cluster, k = 6)
+table(sub_grp)
+#smp_totalDF %>% mutate(cluster = sub_grp) %>% head
 
-################################################################################
+#2.7 show the clustering of the data, using the first two principle components 
+#Die ersten beiden Hauptkomponenten drücken Achsen im n-dimensionalen Merkmalsraum 
+#mit der höchsten Variabilität aus -> lassen sich keinen Variablen mehr zuordnen,
+#aber sind die beste Representation der Datenverteilung
+fviz_cluster(list(data = smp_totalDF, cluster = sub_grp))
+
+#2.8 analyze the best number of groups by comparing the clustering plots
+sub_grp <- cutree(cluster, k = 2)
+p2<- fviz_cluster(list(data = smp_totalDF, cluster = sub_grp)) + ggtitle("k = 2")
+sub_grp <- cutree(cluster, k = 3)
+p3<- fviz_cluster(list(data = smp_totalDF, cluster = sub_grp)) + ggtitle("k = 3")
+sub_grp <- cutree(cluster, k = 4)
+p4<-fviz_cluster(list(data = smp_totalDF, cluster = sub_grp)) + ggtitle("k = 4")
+sub_grp <- cutree(cluster, k = 5)
+p5<-fviz_cluster(list(data = smp_totalDF, cluster = sub_grp)) + ggtitle("k = 5")
+sub_grp <- cutree(cluster, k = 6)
+p6<-fviz_cluster(list(data = smp_totalDF, cluster = sub_grp)) + ggtitle("k = 6")
+
+gridExtra::grid.arrange(p2, p3, p4, p5, p6, nrow = 2, top = "Hierarchisches Clusterverfahren, sample = 100\nUnterschiedliche Klassenzahl dargestellt auf 1. und 2. Hauptkomponente")
+
+#2.9 analyze the optimal number of groups using statistical methods
+#2.9.1 Ellbow method
+fviz_nbclust(smp_totalDF, FUN = hcut, method = "wss")
+#2.9.2 Silhouette method
+fviz_nbclust(smp_totalDF, FUN = hcut, method = "silhouette")
+#2.9.3 Gap method
+gap_stat <- clusGap(smp_totalDF, FUN = hcut, nstart = 25, K.max = 10, B = 50)
+fviz_gap_stat(gap_stat)
 
 #-------------------------------------------------------------------------------
 #3 CLUSTER ANALYSIS - PARTITIONING  CLUSTERING
 #-------------------------------------------------------------------------------
-#load functions
+#see:
+# - https://uc-r.github.io/kmeans_clustering
+# - Handl, A., & Kuhlenkasper, T. (2017). Multivariate Analysemethoden. https://doi.org/10.1007/978-3-662-54754-0 (Kapitel 13 - Clusteranalyse)
+
+#Silhouette calculation function
 source(paste0(workingDir, "CloudType_and_Precipitation_Analysis_Repository/FUN_Silhouetten.R"))
-
-#3.1 Example
-#-----------
-#load data
-alter <- c(43,38,6,47,37,9)
-
-#k-means function
-#x........matrix of the data
-#center...the center and the number of the classes, must be predefined 
-#kmeans(x = matrix(alter,6,1), centers = 2)
-e <- kmeans(x = matrix(alter,length(alter),1), center = matrix(alter[1:2],2,1))
-
-dm <- full(dist(alter))
-
-#Mittelwerte der Klassen
-e$centers
-
-#welches Element in welcher Klasse
-e$cluster
-
-#innerhalb der Gruppen-Streuung
-e$withinss
-
-#größe der Klassen
-e$size
-
-#Berechnen der Silhouetten Werte mit der abgewandelten Silhouetten-Funktion
-sil_params<- silhouette_params(cluster = e, distMat = dm, dat = alter)
-
-#zeichnen der Silhouetten
-plotsilho <- function(silinfo){
-  #reversed s(i) values
-  S <- rev(silinfo[[1]][, 3])
-  #space between groups
-  space <- c(0, rev(diff(silinfo[[1]][,1])))
-  space[space == -1] <- 1
-  #names
-  names <- rev(dimnames(silinfo[[1]])[[1]])
-  if(!is.character(names))
-    names <- as.character(names)
-  barplot(S, space = space, names = names,
-          xlab = "Breite der Silhouette", ylab = "",
-          xlim = c(min(0, min(S)), 1), horiz = T,
-          mgp = c(2.5, 1, 0))
-  invisible()
-}
-
-plotsilho(sil_params)
-
-#Silhouetten Werte in Abhängigkeit von der Klassenzahl
-#Klassenzahl mit dem höchsten Wert entspricht der richtigen Klassenzahl
-#Klassenzahl steigt auf wie folgt:
-#1. 2Klassen
-#2. 3Klassen ...
-#si Werte, benötigte Anzahl gleich Variablenlänge -2 (da alle in einer Klasse und 
-# ein Werte in jeweils einer Klasse nicht untersucht werden)
-si <- rep(0,length(alter)-2)
-
-#iteration über oben genannte Klassenzahlen
-for(i in 2:(length(alter)-1)){
-  e_g <- kmeans(x = matrix(alter,length(alter),1),
-         center = matrix(alter[1:i],i,1))
-  si[i-1]<-silhouette_params(cluster = e_g, distMat = dm, dat = alter)[[3]]
-}
-si
 
 #3.2 Application solely with precipitation data
 #----------------------------------------------
@@ -337,7 +236,7 @@ p4 <- fviz_cluster(cluster_4, geom = "point", data = smp_totalDF) + ggtitle("k =
 p5 <- fviz_cluster(cluster_5, geom = "point", data = smp_totalDF) + ggtitle("k = 5")
 p6 <- fviz_cluster(cluster_6, geom = "point", data = smp_totalDF) + ggtitle("k = 6")
 
-grid.arrange(p2, p3, p4, p5, p6, nrow = 2)
+grid.arrange(p2, p3, p4, p5, p6, nrow = 2, top = "Partitionierendes Clusterverfahren (kmeans), sample = 100\nUnterschiedliche Klassenzahl dargestellt auf 1. und 2. Hauptkomponente")
 
 fviz_cluster(cluster, data = smp_totalDF)
 
